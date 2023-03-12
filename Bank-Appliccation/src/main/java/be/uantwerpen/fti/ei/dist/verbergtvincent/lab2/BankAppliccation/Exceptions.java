64785737fecf360c20f6b80ac3b6270aff8c1007
vincent verbergt <vincent.verbergt@gmail.com>
2023-03-12 21:55:16 +0100

    added Insufficient funds exception

--- Bank-Appliccation/src/main/java/be/uantwerpen/fti/ei/dist/verbergtvincent/lab2/BankAppliccation/Exceptions.java
+++ Bank-Appliccation/src/main/java/be/uantwerpen/fti/ei/dist/verbergtvincent/lab2/BankAppliccation/Exceptions.java
@@ -1,7 +1,13 @@
 package be.uantwerpen.fti.ei.dist.verbergtvincent.lab2.BankAppliccation;
 
-class OwnerException extends Exception{
+class OwnerException extends Exception {
     OwnerException(String message) {
+        super(message);
+    }
+}
+
+class InsufficientFundsException extends Exception {
+    InsufficientFundsException(String message) {
         super(message);
     }
 }
