e50e5a754d93c57ae83a53cb5c65c9aba421fe51
vincent verbergt <vincent.verbergt@gmail.com>
2023-03-12 21:28:17 +0100

    Created User class

---
+++ Bank-Appliccation/src/main/java/be/uantwerpen/fti/ei/dist/verbergtvincent/lab2/BankAppliccation/User.java
@@ -0,0 +1,33 @@
+package be.uantwerpen.fti.ei.dist.verbergtvincent.lab2.BankAppliccation;
+
+import java.util.ArrayList;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+public class User {
+    private String id = UUID.randomUUID().toString();
+    private String name;
+    private ArrayList<BankAccount> accounts;
+
+
+    public User(String name) {
+        this.name = name;
+        this.accounts = new ArrayList<BankAccount>();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<String> getAccounts() {
+        return accounts.stream().map(account -> account.getId()).collect(Collectors.toCollection(ArrayList::new));
+    }
+}
