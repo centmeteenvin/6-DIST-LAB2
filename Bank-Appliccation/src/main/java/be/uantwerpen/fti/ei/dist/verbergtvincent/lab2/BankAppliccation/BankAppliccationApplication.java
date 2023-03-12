3a0fb0c83d01b9513b09da8a8d21e3fb335a905f
vincent verbergt <vincent.verbergt@gmail.com>
2023-03-12 21:06:49 +0100

    init commit

---
+++ Bank-Appliccation/src/main/java/be/uantwerpen/fti/ei/dist/verbergtvincent/lab2/BankAppliccation/BankAppliccationApplication.java
@@ -0,0 +1,13 @@
+package be.uantwerpen.fti.ei.dist.verbergtvincent.lab2.BankAppliccation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAppliccationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAppliccationApplication.class, args);
+	}
+
+}
---
+++ Bank-Appliccation/src/test/java/be/uantwerpen/fti/ei/dist/verbergtvincent/lab2/BankAppliccation/BankAppliccationApplicationTests.java
@@ -0,0 +1,13 @@
+package be.uantwerpen.fti.ei.dist.verbergtvincent.lab2.BankAppliccation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAppliccationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
