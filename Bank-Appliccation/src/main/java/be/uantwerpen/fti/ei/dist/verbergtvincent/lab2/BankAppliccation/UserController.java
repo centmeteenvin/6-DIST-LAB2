afefa177291114f9c7d7c1d6af5728fa51899913
vincent verbergt <vincent.verbergt@gmail.com>
2023-03-13 14:19:04 +0100

    init commit

---
+++ Bank-Appliccation/src/main/java/be/uantwerpen/fti/ei/dist/verbergtvincent/lab2/BankAppliccation/UserController.java
@@ -0,0 +1,8 @@
+package be.uantwerpen.fti.ei.dist.verbergtvincent.lab2.BankAppliccation;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+}
